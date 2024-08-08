888d1167f094f39594ebd13870d4d9f55cd0295a
Diego Sá <diego.sa1687@hotmail.com>
2024-08-08 12:16:03 -0300

    CONSTRUÇÃO DO BACKEND

---
+++ versao-inicial/backend/knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+    client: 'postgresql',
+    connection: {
+      database: 'knowledge',
+      user:     'postgres',
+      password: '12345'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+};
